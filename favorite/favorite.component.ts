8132ed31dc15c26bd80c8652913cbb6cb76f9dc5
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2019-12-27 14:33:04 -0500

    Add files via upload
    Event handling  using Font Awesome

---
+++ favorite/favorite.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent {
+  @Input('isFavorite') isSelected: boolean;
+
+  @Output('change') click = new EventEmitter();
+
+  constructor() { }
+
+  
+
+  onClick(){
+    this.isSelected = !this.isSelected;
+    this.click.emit({ newValue: this.isSelected });
+  }
+  
+}
