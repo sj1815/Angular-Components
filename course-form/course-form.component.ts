59563863d6ce4989d44f101b20424db3a20e018f
Saurabh Joshi <43156540+sj1815@users.noreply.github.com>
2019-12-27 16:49:32 -0500

    Add files via upload

---
+++ course-form/course-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-form',
+  templateUrl: './course-form.component.html',
+  styleUrls: ['./course-form.component.css']
+})
+export class CourseFormComponent  {
+categories  = [
+  {id : 1, name : "Development"},
+  {id : 2, name : "Art" },
+  {id : 3, name : "Languages" }
+];
+
+onSubmit(course){
+  console.log(course);
+}
+
+}
